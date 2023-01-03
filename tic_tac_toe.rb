68646af972239027ea52de7d168ccd20cbfc9ec0
Dylan Palmboom <nofxboy1234@gmail.com>
2023-01-03 17:14:22 +1000

    Determine winner using predefined winning lines

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -6,6 +6,17 @@ require 'pry-byebug'
 class Board
   attr_accessor :board_markers
 
+  LINES = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
   def initialize
     @board_markers = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     display_board
@@ -18,6 +29,18 @@ class Board
     puts '---+---+---'
     puts " #{@board_markers[6]} | #{@board_markers[7]} | #{@board_markers[8]} "
   end
+
+  def winner?
+    # binding.pry
+
+    winner = false
+    LINES.each do |line|
+      if @board_markers[line[0]] == @board_markers[line[1]] && @board_markers[line[0]] == @board_markers[line[2]]
+        winner = true
+      end
+    end
+    winner
+  end
 end
 
 # Player class
@@ -26,9 +49,9 @@ class Player
   @count = 0
 
   attr_accessor :marker
+  attr_reader :name
 
   def initialize(marker, name)
-    # binding.pry
     @marker = marker
     @name = name
     @@players << self
@@ -41,6 +64,10 @@ class Player
     p @count
   end
 
+  def self.players
+    @@players
+  end
+
   class << self
     attr_reader :count
   end
@@ -50,35 +77,19 @@ class Player
   end
 end
 
-# create board
 board = Board.new
 
-# display board:
-#  0 | 1 | 2
-# ---+---+---
-#  3 | 4 | 5
-# ---+---+---
-#  6 | 7 | 8
-# ---+---+---
+Player.new('X', 'Player 1')
+Player.new('O', 'Player 2')
+players = Player.players.cycle
 
-# create player 1 - 'X'
-player_one = Player.new('X', 'Player 1')
-# create player 2 - 'O'
-player_two = Player.new('O', 'Player 2')
-Player.show_players
+until board.winner?
+  player = players.next
+  puts "Please choose a position to place your #{player.marker} marker"
+  position = gets.chomp.to_i
+  puts "You chose position #{position}"
+  board.board_markers[position] = player.marker
+  board.display_board
+end
 
-# While no winning line:
-# Ask player 1 for a position to place marker
-puts 'Please choose a position to place your X marker'
-position = gets.chomp
-puts "You chose position #{position}"
-# Update board
-board.board_markers[position.to_i] = player_one.marker
-# Display board with marker
-board.display_board
-
-# Check if there's a winning line
-
-# Ask player 2 for a position to place marker
-# Display board with marker
-# Check if there's a winning line
+puts "#{player.name} (#{player.marker}) wins!"
