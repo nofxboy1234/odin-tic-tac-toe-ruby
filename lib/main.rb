011f45d7b12bb2645d10d22c893e6283fde52d6f
Dylan Palmboom <nofxboy1234@gmail.com>
2023-05-15 11:07:49 +0200

    Create Game class

--- lib/main.rb
+++ lib/main.rb
@@ -1,32 +1,5 @@
 # frozen_string_literal: true
 
-require './lib/tic_tac_toe'
+require './lib/game'
 
-board = Board.new
-
-Player.new('X', 'Player 1')
-Player.new('O', 'Player 2')
-players = Player.players.cycle
-
-player = players.next
-
-play_game = 'y'
-until play_game == 'n'
-  board.reset
-
-  until board.winner?
-    board.display_board
-    puts "Please choose a position (0-8) to place your #{player.marker} marker"
-    position = gets.chomp.strip
-    next unless board.valid_position?(position)
-
-    board.markers[position.to_i] = player.marker
-    player = players.next unless board.winner?
-  end
-
-  board.display_board
-  puts "\n#{player.name} (#{player.marker}) wins!"
-
-  puts "\nPlay again? (y/n)"
-  play_game = gets.chomp.strip
-end
+Game.new.play
