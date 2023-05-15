6cbf8106927d4018c64d2d2b162bb0768a2dadec
Dylan Palmboom <nofxboy1234@gmail.com>
2023-05-15 11:28:41 +0200

    Test Board#reset

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
-require './lib/tic_tac_toe'
+require './lib/board'
 
 RSpec.describe Board do
+  subject(:board) { Board.new }
+
   describe '#reset' do
-    
+    it 'sets @markers' do
+      board.reset
+
+      expect(board.markers).to eq([0, 1, 2, 3, 4, 5, 6, 7, 8])
+    end
   end
 end
